930ba0222e205ca468c5c9954ed8626e6cde646c
Eder Oliveira <ederco2@gmail.com>
2020-01-29 14:21:53 -0300

    adicionando public no proprieties do test

--- src/test/java/com/kazale/pontointeligente/api/PontoInteligenteApplicationTests.java
+++ src/test/java/com/kazale/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -10,10 +10,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
-class PontoInteligenteApplicationTests {
+public class PontoInteligenteApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public  void contextLoads() {
 	}
 
 }
